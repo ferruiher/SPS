306132fa8eeae21d501f40e6566340d540d015eb
Ubu <frg0086@gmail.com>
2018-02-19 11:39:22 +0100

    primera muestra de imagenes

--- SPS.js
+++ SPS.js
@@ -4,7 +4,8 @@ var path = require('path');
 var bodyParser = require('body-parser');
 const https = require('https');
 var request = require('request');
-
+var picArray = [];
+//const picJsons = {};
 
 
 // app.set('views',path.join(__dirname, 'Mypexel'));
@@ -14,20 +15,34 @@ app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }));
 app.use(bodyParser.json({ limit: '5mb' }));
 
 app.get('/', (req, res) =>{
-    res.render('home')
+    res.render('home', {picArray: picArray})
 })
 
 app.post ('/post/search' ,(req,res)=>{
   var rec = req.body.search;
   var apiKey = '8047832-4eb74703bfe5535846a8f3959'
-  var url = 'https://pixabay.com/api/?key=' +apiKey+ '&q=' +rec + '&image_type=all';
+  var url = 'https://pixabay.com/api/?key=' +apiKey+ '&q=' +rec + '&image_type=all' ;
   
   request (url, (error,response,body)=>{
     
-    console.log(JSON.parse(body));
+    var picJsons = JSON.parse(body);
+    //var picArray = [];
+    //console.log(picJsons);
+    
+    if (parseInt(picJsons.totalHits) > 0){
+      var i = 0;
+      picJsons.hits.forEach(element => {
+        picArray.push(picJsons.hits[i].previewURL);
+        i++
+      });
+    }
+    else
+      console.log ('Noooo pictures');
+    
+    //res.send(picArray);
+    res.render('home', {picArray: picArray})
   })
 
-//   res.send(url);
 })
 
 app.listen (8082)
