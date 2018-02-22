2b1f734b1fb9b78838f15a9927fc5d301d9637a6
Ubu <frg0086@gmail.com>
2018-02-22 20:38:23 +0100

    bajada de imagen e imagenes

--- SPS.js
+++ SPS.js
@@ -2,10 +2,15 @@ var espress =  require('express');
 var app = espress();
 var path = require('path');
 var bodyParser = require('body-parser');
-const https = require('https');
+var https = require('https');
 var request = require('request');
 var array = require('array');
+var formidable = require('formidable');
+var fs = require ('fs');
+var download = require('image-downloader');
 
+const FILEPATH = "/home/ubu/Descargas/tem"
+const SAVEPATH = '/home/ubu/Imágenes/'
 
 var picArray = [];
 //const picJsons = {};
@@ -51,19 +56,63 @@ app.post ('/post/search' ,(req,res)=>{
 })
 
 app.post('/post/download', (req, res)=>{
-  console.log(req.body);
-  var pepe = 'pepe';
-  var download = req.body.ids;
+  // console.log(req.body);
+  // var pepe = 'pepe';
+  var downloadUrl = req.body.ids;
+  console.log (downloadUrl);
   console.log ("---------------------");
   
-  if (typeof download === 'string'){
+  if (typeof downloadUrl === 'string'){
     console.log("Es un String");
+    var options = {
+      url: downloadUrl,
+      dest: FILEPATH
+    }
     
-
+  download.image(options).then((result)=>{
+    var filename = result.filename.substr(24, result.filename.length - 1 );
+    // console.log(filename + '--------------------------------------------------------------------------------------------------------------');
+    var oldpath = result.filename;
+    var newpath = SAVEPATH + filename;
+    // console.log ('the newpath is: '+ newpath+ '+++++++++++++++++++++++++');
+    fs.rename(oldpath, newpath, (err)=>{
+      if (err) throw err;
+      res.write('File upload and moved');
+      res.end;
+    })
+  }).catch((err)=>{
+    console.log("download error:")
+    console.log(err)
+  });
+   
   }else{
     console.log("Es un ARARY");
+    
+    downloadUrl.forEach(element => {
+      
+      var options = {
+        url: element,
+        dest: FILEPATH
+      }
+      
+    download.image(options).then((result)=>{
+      var filename = result.filename.substr(24, result.filename.length - 1 );
+      // console.log(filename + '--------------------------------------------------------------------------------------------------------------');
+      var oldpath = result.filename;
+      var newpath = SAVEPATH + filename;
+      // console.log ('the newpath is: '+ newpath+ '+++++++++++++++++++++++++');
+      fs.rename(oldpath, newpath, (err)=>{
+        if (err) throw err;
+        res.write('File upload and moved');
+        // console.log('File upload and moved');
+        res.end;
+      })
+    }).catch((err)=>{
+      console.log("download error:")
+      console.log(err)
+    });
+    });
   }
-  console.log (download);
   //res.send(req.body.ids);
   // res.render('home', {results: []})
 })
