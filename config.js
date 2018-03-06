4940a3f4ae3350f113bd92f1fc8babf2e9f4f5a9
Ubu <frg0086@gmail.com>
2018-03-06 14:52:45 +0100

    añadido configuracón, cambio de pc

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    server:{
+        portApi : 3082,
+        portUI : 3083,
+    },
+    paths:{
+        FILEPATH : '/home/ubu/Descargas/tem/',
+        SAVEPATH : '/home/ubu/Imágenes/'
+
+    }
+}
